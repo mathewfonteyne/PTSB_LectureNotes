4c8c6de57f30aed246770faec55394a1fd590a44
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-02-21 20:31:17 -0500

    Finished Array Destructuring notes, and started object and object methods notes

--- Unit_3/arrayDestructuring.js
+++ Unit_3/arrayDestructuring.js
@@ -88,3 +88,77 @@ persons.push({name: "Anna", age: 29});
 console.log("Persons Array: ", persons); // Includes Anna object in array
 
 console.log("Copied Persons Array: ", copiedPersons); // Copy does not include Anna, created before Anna was added
+
+
+// * Variables within Memory
+// First assignment/original assignment
+let x = 10;
+let y= "abc";
+let z = null;
+
+// Reassign to another variable
+let a = x;
+console.log(a); // 10
+let b = y;
+console.log(b); // abc
+
+// Reassign a and b
+a = 5;
+b = "def";
+
+console.log(x, y, a, b); // 10, abc, 5, def
+
+// ---------------------------------
+
+let arr = [];
+arr.push(1);
+console.log(`arr: ${arr}`);
+
+let reference = [1];
+let refCopy = reference;
+console.log(`refCopy: ${refCopy}`);
+
+reference.push(2); // both reference and refCopy will have the 2 added
+console.log(reference, refCopy); // [ 1, 2 ] [ 1, 2 ]
+
+// Look up what is going on with .push()
+refCopy.push(3);
+console.log(reference, refCopy); // What I HAD expected [ 1, 2 ] [ 1, 2, 3 ] 
+// What I got [ 1, 2, 3 ] [ 1, 2, 3 ]
+
+// ---------------------------------
+//? Rest Syntax
+/* 
+  We can use the spread operator syntax, referred to as 'rest' syntax when working with array destructuring, to package up the REST of the values in the referenced array, and return them as their own array.
+
+  - Looks exactly like the spread syntax: ...
+  - Spread -> "expands" an array
+  - Rest -> "condenses" an array
+*/
+
+const fullNameAgain = [
+  "Jane",
+  "Doe",
+  "Mrs.",
+  {
+    month: 03,
+    date: 22,
+    year: 1973,
+  },
+  2,
+  "test",
+  "4",
+  true,
+  false,
+];
+
+let janesName = fullNameAgain[0];
+// console.log(janesName);
+
+// Using both array destructuring and rest:
+// let [newFirstName, newLastName, ...otherInfo] = fullNameAgain;
+let [newFirstName, newLastName, , ...otherInfo] = fullNameAgain;
+console.log(`newFirstName: ${newFirstName}`); // Jane
+console.log(`newLastName: ${newLastName}`); // Doe
+console.log(`otherInfo: ${otherInfo}`); // otherInfo: [object Object],2,test,4,true,false
+console.log(otherInfo); // [ { month: 3, date: 22, year: 1973 }, 2, 'test', '4', true, false ]
