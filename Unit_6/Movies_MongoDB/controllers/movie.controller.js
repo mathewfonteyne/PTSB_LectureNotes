2cc3317fddb91e0f81f42979e3792d9822ec27e0
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-04-16 19:03:08 -0400

    Started CRUD Methods for Movie Database

---
+++ Unit_6/Movies_MongoDB/controllers/movie.controller.js
@@ -0,0 +1,144 @@
+const router = require("express").Router();
+
+const Movie = require("../models/movie.model");
+
+// Error Response Function
+const errorResponse = (res, error) => {
+  return res.status(500).json({
+    Error: error.message,
+  });
+};
+
+//TODO POST
+router.post("/", async (req, res) => {
+  // http://localhost:4000/movies/
+  try {
+    //1. Pull data from client (body)
+    const { title, genre, rating, length, releaseYear } = req.body;
+    //2. Create a new object using the Model
+    const movie = new Movie({
+      //   title: req.body.title,
+      //   genre: req.body.genre,
+      //   rating: req.body.rating,
+      //   length: req.body.length,
+      //   releaseYear: req.body.releaseYear,
+      title,
+      genre,
+      rating,
+      length,
+      releaseYear,
+    });
+    //3. Use mongoose method to save to MongoDB
+    const newMovie = await movie.save();
+    //4. client response
+    res.status(200).json({
+      movie: newMovie,
+      message: "Success! New Movie Added",
+    });
+  } catch (err) {
+    errorResponse(res, err);
+  }
+});
+
+//TODO GET One
+/* 
+   Challenge
+        - By ID
+        - Response should consider
+            - If found
+            - If no document found
+        
+        Hint: Consider login within user.controller.js
+
+*/
+
+router.get("/:id", async (req, res) => {
+  // http://localhost:4000/movies/643c61a63353c1729ba83387
+  try {
+    // grab parameters from url
+    const { id } = req.params;
+
+    // Find the document (movie JSON obj) within the DB Collection
+    const getMovie = await Movie.findOne({ _id: id });
+
+    // Response message
+    if (!movie) throw new Error("No Movie Found!");
+    res.status(200).json({
+      message: `${getMovie.title} was found!`,
+      getMovie,
+    });
+  } catch (err) {
+    errorResponse(res, err);
+  }
+});
+//TODO Get All
+/* 
+   Challenge
+        - No special endpoint necessary
+        - Response should consider
+            - If found
+            - not found
+        
+        
+        Hint: parameters within method are optional
+*/
+
+router.get("/", async (req, res) => {
+  //  http://localhost:4000/movies/
+  try {
+    // This endpoint will only return all movies
+    // Await all documents from the movie collection
+    const getAllMovies = await Movie.find();
+    if (!getAllMovies) throw new Error("No Movies Found");
+    res.status(200).json({
+      message: "All Movies!",
+      getAllMovies,
+    });
+  } catch (err) {
+    errorResponse(res, err);
+  }
+});
+//TODO Get All by Genre
+/* 
+!   Challenge
+        - No special endpoint necessary
+        - Response should consider
+            - If found
+            - not found
+        - Consider parameter casing within the endpoint.
+            - hint: conditional w/ looping
+*/
+
+router.get("/genre/:genre", async (req, res) => {
+  try {
+    // Grab genre value from parameters
+    const { genre } = req.params;
+
+    // Trying to spell genres the same way...
+    // if (genre) {
+    //   for (let i = 0; i < genre.length; i++) {
+    //     i === 0
+    //       ? (buildWord = genre[i].toUpperCase())
+    //       : (buildWord += genre[i].toLowerCase());
+    //   }
+    // }
+
+    // Finding all movies in DB whose genre matches the params ({db genre key: req.params.genre})
+    const getMovies = await Movie.find({ genre: genre });
+
+    getMovies.length > 0
+      ? res.status(200).json({
+          getMovies,
+        })
+      : res.status(404).json({
+          message: `No movies found.`,
+        });
+  } catch (err) {
+    errorResponse(res, err);
+  }
+});
+//TODO PATCH One
+
+//TODO Delete One
+
+module.exports = router;
