8fab9f3f46446c1ee85c89e5e46bbdffa8906f26
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-05-04 20:58:41 -0400

    Created another example for building props and components.

---
+++ Unit_7/starting-react/src/components/Presidents/DisplayTotals.jsx
@@ -0,0 +1,11 @@
+function DisplayTotals(props) {
+  return (
+    <>
+      <p>
+        {props.name} had a total vote count of {props.count}
+      </p>
+    </>
+  );
+}
+
+export default DisplayTotals;
---
+++ Unit_7/starting-react/src/components/Presidents/PresidentVotes.jsx
@@ -0,0 +1,19 @@
+import DisplayTotals from "./DisplayTotals";
+function PresidentVotes() {
+  const votes = [
+    { name: "Abe Lincoln", count: 139033 },
+    { name: "Stephen Douglas", count: 115509 },
+  ];
+
+  return (
+    <>
+      <h1>Presidents: </h1>
+      {/* Need to map votes into DisplayTotals */}
+      {votes.map((pres, index) => (
+        <DisplayTotals id={index} name={pres.name} count={pres.count} />
+      ))}
+    </>
+  );
+}
+
+export default PresidentVotes;
