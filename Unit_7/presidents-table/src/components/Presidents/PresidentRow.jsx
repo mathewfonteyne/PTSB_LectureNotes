2a0a86e8b8006b690ff961e5a7adff85214f052f
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-05-11 20:30:43 -0400

    Made a presidents row and table react app

---
+++ Unit_7/presidents-table/src/components/Presidents/PresidentRow.jsx
@@ -0,0 +1,17 @@
+// This will be child to PresidentTable and imported into it.
+
+export default function PresidentRow({ first, last, year, passed }) {
+  let date = new Date().getFullYear();
+
+  return (
+    <>
+      <tr>
+        <td>{first}</td>
+        <td>{last}</td>
+        <td>{year}</td>
+        <td>{passed === undefined ? "Yes" : "No"}</td>
+        <td>{passed === undefined ? date - year : passed - year}</td>
+      </tr>
+    </>
+  );
+}
---
+++ Unit_7/presidents-table/src/components/Presidents/PresidentTable.jsx
@@ -0,0 +1,34 @@
+// This will be a table component, child of Presidents, parent to PresidentRow
+import PresidentRow from "./PresidentRow";
+
+export default function PresidentTable(props) {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Birth Year</th>
+            <th>Alive</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.presidents.map((president, i) => {
+            return (
+              <PresidentRow
+                key={i}
+                first={president.first}
+                last={president.last}
+                year={president.year}
+                passed={president.passed}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
